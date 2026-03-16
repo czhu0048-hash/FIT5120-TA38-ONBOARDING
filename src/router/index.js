cb720353d0a4c5df7030aee9fd380cbc36ba22b7
czhu0048 <czhu0048@student.monash.edu>
2026-03-16 15:54:40 +1100

    Fix: removed one line from index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import ForYouView from '../views/ForYouView.vue'
 import AwarenessView from '@/views/AwarenessView.vue'
 import AwarenessViewOne from '@/views/AwarenessViewOne.vue'
 import DashboardView from '../views/DashboardView.vue'
-import HomeView from '@/views/HomeView.vue'
 import WelcomeView from '@/views/WelcomeView.vue'
 
 const routes = [
